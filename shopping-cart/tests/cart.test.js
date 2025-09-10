20db7a2e7038d1e26061d639250c976210ddd9a5
DeepthiGirijaThampi <deepthig01@gmail.com>
2025-09-10 10:49:17 -0500

    completed the unit tests and made a few changes in cart.js

--- shopping-cart/tests/cart.test.js
+++ shopping-cart/tests/cart.test.js
@@ -24,9 +24,52 @@ describe("add",function(){
 
 describe("remove",function(){
 
+    test("should remove an existing item with a valid name and quantity",function(){
+        const cart = [{item : "apple",quantity : 2}];
+        remove(cart,"apple");
+        expect(cart).toEqual([]);
+
+    });
+
+    test("should not remove item not in cart",function(){
+        const cart = [{item : "apple",quantity : 3}];
+        remove(cart,"banana");
+        expect(cart).toEqual([{item : "apple",quantity : 3}]);
+    });
+
+    test("should remove the last item from the cart",function(){
+        const cart = [
+                        {item : "apple",quantity : 3},
+                        {item : "banana",quantity : 2}
+                     ];
+
+        remove(cart,"banana");
+        expect(cart).toEqual([{item : "apple",quantity : 3}]);
+    });
 
 });
 
 describe("getTotal",function(){
+  test("should calculate the total items correctly",function(){
+    const cart = [
+                    {item: "apple", quantity: 2},
+                    {item: "orange", quantity: 3}
+                 ];
+    expect(getTotal(cart)).toEqual(5);
 
+  });
+
+  test("should return 0 for an empty cart ",function(){
+    const cart = [];
+    expect(getTotal(cart)).toEqual(0);
+  });
+
+  test("should calculate the total if there are large quantities",function(){
+    const cart = [
+                    {item: "apple", quantity: 20},
+                    {item: "orange", quantity: 30}
+                ];
+    expect(getTotal(cart)).toEqual(50);
+
+  });
 });
